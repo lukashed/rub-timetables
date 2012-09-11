50b53be218e11d46d5800913f6d79b62372b8be5
Lukas Klein <lukas@lukasklein.com>
2012-09-11 17:54:24 +0200

    Added a customized version of node-icalendar and added day-parsing

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express'),
 	app = express(),
 	request = require('request'),
 	jsdom = require('jsdom'),
-	nurl = require('url');
+	nurl = require('url'),
+	icalendar = require('./node-icalendar');
 
 app.use(express.bodyParser());
 
@@ -11,10 +12,12 @@ app.get('/', function(req, res) {
 		var url = req.query.url;
 		var host = nurl.parse(url).host;
 		var protocol = nurl.parse(url).protocol;
+		var ical = new icalendar.iCalendar();
 		request(url, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				jsdom.env(body, ['http://code.jquery.com/jquery-1.8.1.min.js'], function (errors, window) {
 					var tds = window.$('td');
+					var cnt = 0, cnt2 = 0;
 					tds.each(function() {
 						if (window.$('a', this).length == 2) {
 							var title = window.$(window.$('a strong', this)[0]).text();
@@ -26,7 +29,8 @@ app.get('/', function(req, res) {
 							var link = protocol + '//' + host + window.$(window.$('a', this)[0]).attr('href');
 
 							jsdom.env(link, ['http://code.jquery.com/jquery-1.8.1.min.js'], function (errors, window) {
-								window.$('ul.dates li').each(function() {
+								var dates = window.$('ul.dates li');
+								dates.each(function() {
 									var cont = window.$.trim(window.$(this).text());
 									var timestart = time.split('-')[0];
 									var timeend = time.split('-')[1];
@@ -47,15 +51,31 @@ app.get('/', function(req, res) {
 											day = 4;
 										}
 										if (day) {
+											var event = ical.addComponent('VEVENT');
+											event.setSummary(title + '(' + type + ')');
+											event.setDescription('bei ' + prof);
+											event.setUrl(link);
+											event.setLocation(location);
+											var hours = timeend.split(':')[0] - timestart.split(':')[0];
+											var minutes = 60 - timeend.split(':')[1] - timestart.split(':')[1];
+											console.log(timeend.split(':')[1], timestart.split(':')[1]);
+											console.log(hours, minutes);
+											event.setDate(new Date(2012, 9, day + 1, timestart.split(':')[0], timestart.split(':')[1], 0), 60 * ((60 * hours) + minutes));
 											console.log(title, type, location, link, day);
 										}
 									}
 								});
+								cnt2++;
+								if (cnt2 === cnt) {
+									console.log(ical.toString());
+								}
 							});
+							cnt++;
 						}
 					});
 				});
 			}
+
 		});
 	}
 	res.send('hello world');
